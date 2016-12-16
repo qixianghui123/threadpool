f98a3d28b9fc79aa0afdcc514055bf614836a2f6
root <fish>
2016-12-16 10:53:40 +0800

    thread pool

---
+++ ThreadPool.h
@@ -0,0 +1,35 @@
+#ifndef THREAD_POOL_H_
+#define THREAD_POOL_H_
+
+#include "Task.h"
+
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+class ThreadPool
+{
+				public:
+								ThreadPool();
+								~ThreadPool();
+								void Create();
+								void AddTask(Task *task);
+				private:
+								static void* routine(void *s);
+								void AddToIdleList(pthread_t tid);
+								void MoveToIdleList();
+								void MoveToBusyList();
+				private:
+								static vector<Task*>		m_task_v;
+								static pthread_mutex_t	m_mutex;
+								static pthread_cond_t		m_cond;
+
+								vector<pthread_t>				m_idle_list;
+								vector<pthread_t>				m_busy_list;
+
+								pthread_mutex_t					m_idle_mutex;
+								pthread_mutex_t					m_bust_mutex;
+};
+#endif
